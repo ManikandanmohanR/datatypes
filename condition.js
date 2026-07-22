038c3748ff7cc30e3d31775eb64cb75e75ca9997
Manikandan M <maniit3060@gmail.com>
2026-07-22 15:26:06 +0530

    create the conditional statement

---
+++ condition.js
@@ -0,0 +1,11 @@
+// Task 1 ---> Employee Login System
+
+
+let username = "admin";
+let password = "12345";
+
+if (username === "admin" && password === "12345") {
+    console.log("Login Successful");
+} else {
+    console.log("Invalid Username or Password");
+}
